f42875129a5e1f0daf26e270b4a88c79a7e42f9f
buncy <karthik138105@gmail.com>
2021-04-12 22:24:21 +0530

    first commit

---
+++ exercises/variables/variables6.rs
@@ -0,0 +1,7 @@
+// variables6.rs
+// Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
+
+fn main() {
+    const NUMBER: i32 = 3;
+    println!("Number {}", NUMBER);
+}
